2927a67870cfd1dc5b869351bfef0e52c6854b42
yagi <yagi@JISCALD>
2020-05-06 17:00:09 +0900

    [バグ 00001] 対応

--- src/dssp/hashidate/misc/Pair.java
+++ src/dssp/hashidate/misc/Pair.java
@@ -1,66 +1,55 @@
 package dssp.hashidate.misc;
 
+import java.util.Objects;
+
 /*
  * 名前とオブジェクトの組
  */
-public class Pair
-{
-	private String name;
-	private Object value;
-	public static final String SEPARATOR = "=";
+public class Pair {
+    private String name;
+    private Object value;
+    public static final String SEPARATOR = "=";
 
-	public Pair(String name, Object value)
-	{
-		this.name = name;
-		this.value = value;
-	}
+    public Pair(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
 
-	public String getName()
-	{
-		return name;
-	}
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	public Object getValue()
-	{
-		return value;
-	}
-	public void setValue(Object value)
-	{
-		this.value = value;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String toString()
-	{
-		return String.format("%s%s%s", this.name.toString(), SEPARATOR, (null == this.value ? "": this.value.toString()));
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public static Pair fromString(String text)
-	{
-		if (null == text)
-		{
-			return null;
-		}
-		String[] tokens = text.split(SEPARATOR);
-		switch(tokens.length)
-		{
-		case 0:
-			return null;
-		case 1:
-			return new Pair(tokens[0].trim(), null);
-		default:
-			StringBuilder val = new StringBuilder();
-			for (int i = 1; i < tokens.length; i++)
-			{
-				if (1 < i)
-				{
-					val.append(SEPARATOR);
-				}
-				val.append(tokens[i]);
-			}
-			return new Pair(tokens[0].trim(), val.toString());
-		}
-	}
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return String.format("%s%s%s", this.name.toString(), SEPARATOR,
+                (null == this.value ? "" : this.value.toString()));
+    }
+
+    public static Pair fromString(String text) {
+        if (null == text) {
+            return null;
+        }
+        String name = null;
+        StringBuilder buf = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Objects.isNull(name) && SEPARATOR.equals(String.valueOf(c))) {
+                name = buf.toString();
+                buf.delete(0, buf.length());
+                continue;
+            }
+            buf.append(c);
+        }
+        return new Pair(name.trim(), buf.toString());
+    }
 }
